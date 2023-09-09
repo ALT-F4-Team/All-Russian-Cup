de1ccdf7ebe862e19a95867b9987cb418d73cb1f
Pol_Bor_Mef <ig88acc@mail.ru>
2023-09-09 14:14:19 +0200

    Add files via upload

--- web/app.py
+++ web/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, redirect, url_for
 import pandas as pd
 import json
 
 app = Flask(__name__)
 app.secret_key = 'a66ee1919de09f25451412411bed2fb755f845d70f96bf0a'
 
+questions = ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5", "Question 6", "Question 7"]
 
 @app.route('/download', methods=['GET', 'POST'])
 def download():
@@ -15,10 +16,33 @@ def download():
     df.to_csv(filename, index=False)
     return send_file(filename, as_attachment=True)
 
+@app.route('/processing', methods=['POST'])
+def processing():
+    selected_question = request.form['question']
+    # Here, you can add the logic for processing the selected question.
+    # Perform any processing for the selected question here.
+    # You can redirect to the processing page or create it as needed.
+    return render_template('processing.html', question=selected_question)
 
 @app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Handle the POST request from the dropdown menu
+        selected_question = request.form.get('question')
+        return redirect(url_for('processing', question=selected_question))
+    return render_template('index.html', questions=questions)
+
+@app.route('/questions/<int:question_id>', methods=['GET', 'POST'])
+def question(question_id):
+    selected_question = questions[question_id - 1]
+    return render_template('index.html', questions=questions)
+
+
+@app.route('/visualization', methods=['GET', 'POST'])
 def visualization():
-    df = pd.read_csv('uploads\main_v2.csv')
+    #question = request.form.get('question', questions[0])  # Get the selected question from the form
+
+    df = pd.read_csv('uploads/main_v2.csv')
     df_group = df.groupby('sentiment').count().reset_index()
     grouped_data = df.groupby('cluster').agg({'2d_embeds': list, 'text': list}).reset_index()
 
@@ -44,8 +68,11 @@ def visualization():
         datasets.append(dataset)
 
     return render_template('charts.html', labels=list(df_group['sentiment']), data=list(df_group['text']),
-                           df=df.to_json(), datasets=datasets, mean_group_count=mean_group_count, top_elemnts=top_elemnts, group_count=group_count, max_cluster_count=max_cluster_count)
+                           df=df.to_json(), datasets=datasets, mean_group_count=mean_group_count,
+                           top_elemnts=top_elemnts, group_count=group_count, max_cluster_count=max_cluster_count)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
+
